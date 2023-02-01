a5b6681f21203f8d6bca4a973c02799e3edcf7b0
Mark <mark.nelubin@gmail.com>
2023-02-01 20:25:45 +0800

    generate joke function

---
+++ src/generateJoke.js
@@ -0,0 +1,5 @@
+const generateJoke = () => {
+  return "I don't trust stairs because they're always up to something, and I don't trust lifts because they let you down.";
+};
+
+export default generateJoke;
