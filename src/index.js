db3a735c2aa38fdc4708a6904bdf81af61611cbb
Mark <mark.nelubin@gmail.com>
2023-02-02 15:20:41 +0800

    asset resource loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import generateJoke from "./generateJoke";
 import "./styles/main.scss";
+import laughing from "./assets/laughing.svg";
+
+const laughEl = document.getElementById("laughImg");
+laughEl.src = laughing;
 console.log(generateJoke());
-console.log(1);
